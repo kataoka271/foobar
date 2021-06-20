655d921e52f65decef46f9445e4e2ccf48571a78
Hiroaki Kataoka <k_hir@hotmail.com>
2021-06-20 17:08:57 +0900

    refactor Manga.user.js

--- Manga.user.js
+++ Manga.user.js
@@ -5,29 +5,19 @@
 // @description manga viewer
 // @version     1.0.0
 // @updateURL   https://github.com/kataoka271/foobar/blob/master/Manga.user.js
-// @include     https://loveheaven.net/*
 // @include     https://mangabank.org/*
 // @grant       GM_addStyle
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
 (function () {
-
-  const wheelholdtime = 300; // ms
-  const defaultAspect = 960 / 1400; // px/px
-
-  let $$ = e => document.querySelectorAll(e);
-
-  let dummyWindowOpen = function () {
-    console.log("dummyWindowOpen");
-  }
-  unsafeWindow.open = exportFunction(unsafeWindow, dummyWindowOpen);
 
   GM_addStyle(`
 :root {
   --manga-image-aspect: 0.6857143;
   --manga-pagenum-height: 15px;
-  --manga-page-progress-height: 8px;
-  --manga-sliding-height: calc(var(--manga-page-progress-height) + var(--manga-pagenum-height) + 10px);
+  --manga-seeker-height: 8px;
+  --manga-slider-height: calc(var(--manga-seeker-height) + var(--manga-pagenum-height) + 10px);
 }
 .manga-viewer {
   background-color: rgba(0, 0, 0, 0.85);
@@ -57,23 +47,23 @@
   left: 0;
   height: var(--manga-pagenum-height);
 }
-.manga-page-progress {
+.manga-seeker {
   background-color: rgba(100, 100, 100, 0.85);
   width: 100%;
   cursor: pointer;
 }
-.manga-page-progress > div {
+.manga-seeker > div {
   background-color: rgba(240, 170, 15, 0.85);
   transition: all 300ms 0s ease;
   margin-left: auto;
-  height: var(--manga-page-progress-height);
+  height: var(--manga-seeker-height);
   margin-top: var(--manga-pagenum-height); /* adjust sum to .manga-pagenum height */
 }
-.manga-sliding {
+.manga-slider {
   background-color: black;
   overflow: hidden;
-  width: min(calc(100vw - 20px - 50px), calc(2 * (100vh - var(--manga-sliding-height)) * var(--manga-image-aspect)));
-  height: calc(100vh - var(--manga-sliding-height));
+  width: min(calc(100vw - 20px - 50px), calc(2 * (100vh - var(--manga-slider-height)) * var(--manga-image-aspect)));
+  height: calc(100vh - var(--manga-slider-height));
 }
 .manga-slides {
   transition: all 300ms 0s ease;
@@ -85,7 +75,7 @@
   width: 50%;
   height: 100%;
 }
-.manga-slides > img.manga-slides-wide {
+.manga-slides > img.manga-slides-full {
   width: 100%;
 }
 .manga-slides > img.manga-slides-half {
@@ -93,7 +83,18 @@
 }
 `);
 
-  let siteinfo = [
+  const dummyWindowOpen = () => {
+    console.log("dummyWindowOpen");
+  };
+  unsafeWindow.open = exportFunction(unsafeWindow, dummyWindowOpen);
+
+  if (unsafeWindow.lazyload) {
+    const lazy = unsafeWindow.lazyload();
+    lazy.destroy();
+    console.log("stop lazyload library");
+  }
+
+  const sitelist = [
     {
       pattern: "^https://loveheaven.net/",
       imgs: "div.chapter-content img",
@@ -106,165 +107,262 @@
     }
   ];
 
-  let site = siteinfo.find(si => new RegExp(si.pattern).test(location.href));
+  const defaultAspect = 960 / 1400; // px/px
+  const wheelHoldTime = 300; // ms
+  const $$ = e => document.querySelectorAll(e);
+
+
+  class Slides {
+
+    constructor() {
+      const viewer = document.createElement("div");
+      const wrapper = document.createElement("div");
+      const pagenum = document.createElement("div");
+      const seeker = document.createElement("div");
+      const seekbar = document.createElement("div");
+      const slider = document.createElement("div");
+      const slides = document.createElement("div");
+
+      viewer.className = "manga-viewer";
+      wrapper.className = "manga-wrapper";
+      pagenum.className = "manga-pagenum";
+      seeker.className = "manga-seeker";
+      slider.className = "manga-slider";
+      slides.className = "manga-slides";
+
+      slider.appendChild(slides);
+      seeker.appendChild(seekbar);
+      wrapper.appendChild(seeker);
+      wrapper.appendChild(pagenum);
+      wrapper.appendChild(slider);
+      viewer.appendChild(wrapper);
+
+      this.viewer = viewer;
+      this.pagenum = pagenum;
+      this.seeker = seeker;
+      this.seekbar = seekbar;
+      this.slides = slides;
+      this.numPages = 0;
+      this.slideOffset = 0;
+      this.wheelHoldTimeStamp = 0;
+      this.imageList = [];
+
+      this.viewer.addEventListener("dblclick", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.close();
+      });
+
+      this.seeker.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.seekOffset(e.clientX - e.currentTarget.getBoundingClientRect().left);
+      });
+
+      slider.addEventListener("wheel", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        if (e.timeStamp - this.wheelHoldTimeStamp < wheelHoldTime) {
+          return;
+        }
+        this.wheelHoldTimeStamp = e.timeStamp;
+        if (e.deltaY < 0) {
+          this.goPrev(e.shiftKey);
+        } else if (e.deltaY > 0) {
+          this.goNext(e.shiftKey);
+        }
+      });
+    }
+
+    get elem() { return this.viewer; }
+
+    setOffset(offset) {
+      if (offset < 0) {
+        offset = 0;
+      } else if (offset > this.numPages - 2) {
+        offset = this.numPages - 2;
+      }
+      this.slides.style.transform = "translateX(" + (-50 * (this.numPages - 2 - offset)) + "%)";
+      this.pagenum.innerHTML = (offset + 2) + "/" + this.numPages;
+      this.seekbar.style.width = (100.0 * (offset + 2) / this.numPages) + "%";
+      this.loadImage(offset);
+      this.loadImage(offset + 1);
+      this.loadImage(offset + 2);
+      this.loadImage(offset - 1);
+      this.loadImage(offset - 2);
+      this.fitToImage(this.imageList[offset]);
+      this.slideOffset = offset;
+    }
+
+    seekOffset(pos) {
+      this.setOffset(Math.floor(this.numPages * (1.0 - pos / this.seeker.clientWidth)));
+    }
+
+    getGlobalAspect() {
+      return document.documentElement.style.getPropertyValue("--manga-image-aspect");
+    }
+
+    setGlobalAspect(aspect) {
+      document.documentElement.style.setProperty("--manga-image-aspect", aspect);
+      console.log("set-aspect", aspect);
+    }
+
+    goNext(shift) { this.setOffset(this.slideOffset + (shift ? 1 : 2)); }
+
+    goPrev(shift) { this.setOffset(this.slideOffset - (shift ? 1 : 2)); }
+
+    open() { this.viewer.style.display = "block"; }
+
+    close() { this.viewer.style.display = "none"; }
+
+    addImage(img) {
+      if (img.isFull()) {
+        this.imageList.push(img);
+        this.imageList.push(img);
+        this.numPages += 2;
+      } else {
+        this.imageList.push(img);
+        this.numPages += 1;
+      }
+      this.slides.insertBefore(img.elem, this.slides.firstChild);
+    }
+
+    fitToImage(img) {
+      if (!img.complete) {
+        img.on("load", () => this.fitToImage(img));
+        return;
+      }
+      let aspect = img.getImageAspect();
+      if (aspect > 1) {
+        aspect = aspect / 2;
+      }
+      aspect = Math.round(aspect * 1000) / 1000;
+      if (Math.abs(this.getGlobalAspect() - aspect) < 0.001) {
+        return;
+      }
+      this.setGlobalAspect(aspect);
+    }
+
+    loadImage(offset) {
+      if (offset < 0 || offset > this.imageList.length) {
+        return;
+      }
+      this.imageList[offset].loadLazy();
+    }
+
+    createOpenButton(title) {
+      const button = document.createElement("a");
+      button.innerHTML = title;
+      button.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.open();
+      });
+      return button;
+    }
+
+  }
+
+
+  class WrappedImage {
+
+    constructor(img) {
+      this.img = img;
+      if (this.isFull()) {
+        img.className = "manga-slides-full";
+      } else {
+        img.className = "manga-slides-half";
+      }
+      img.addEventListener("click", () => this.loadForce());
+    }
+
+    get elem() { return this.img; }
+
+    get complete() { return this.img.complete; }
+
+    on(event, func) { this.img.addEventListener(event, func); }
+
+    loadLazy() {
+      const dataSrc = this.img.getAttribute("data-src");
+      const src = this.img.getAttribute("src");
+      if (dataSrc != src) {
+        this.img.src = dataSrc;
+      }
+    }
+
+    loadForce() {
+      const url = this.img.getAttribute("data-src");
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: url,
+        timeout: 5000,
+        responseType: "blob",
+        onload: (xhr) => {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              this.img.src = URL.createObjectURL(xhr.response);
+              this.img.setAttribute("data-src", this.img.src);
+            } else {
+              console.error(xhr.status + " " + xhr.statusText);
+            }
+          }
+        },
+        ontimeout: () => {
+          console.error("The request for " + url + " timed out");
+        }
+      });
+    }
+
+    getImageAspect() {
+      const aWidth = this.img.getAttribute("width");
+      const aHeight = this.img.getAttribute("height");
+      const dataSrc = this.img.getAttribute("data-src");
+      const src = this.img.getAttribute("src");
+      if (aWidth && aHeight) {
+        return parseInt(aWidth) / parseInt(aHeight);
+      } else if (dataSrc == src && this.img.naturalWidth > 0 && this.img.naturalHeight > 0) {
+        return this.img.naturalWidth / this.img.naturalHeight;
+      } else {
+        return defaultAspect;
+      }
+    }
+
+    isFull() {
+      return this.getImageAspect() > 1;
+    }
+
+  }
+
+
+  const site = sitelist.find(it => new RegExp(it.pattern).test(location.href));
   if (!site) {
     return;
   }
-  let imgs = $$(site.imgs);
+  const imgs = $$(site.imgs);
   if (imgs.length == 0) {
     return;
   }
-  let container = $$(site.container)[0];
+  const container = $$(site.container)[0];
   if (!container) {
     return;
   }
-  let viewer = document.createElement("div");
-  viewer.className = "manga-viewer";
-  let wrapper = document.createElement("div");
-  wrapper.className = "manga-wrapper";
-  let pagenum = document.createElement("div");
-  pagenum.className = "manga-pagenum";
-  let pageprogress = document.createElement("div");
-  pageprogress.className = "manga-page-progress";
-  let pageprogressbar = document.createElement("div");
-  let sliding = document.createElement("div");
-  sliding.className = "manga-sliding";
-  let getAspect = function (img) {
-    let aWidth = img.getAttribute("width");
-    let aHeight = img.getAttribute("height");
-    let dataSrc = img.getAttribute("data-src");
-    let src = img.getAttribute("src");
-    let aspect = defaultAspect;
-    if (aWidth && aHeight) {
-      aspect = parseInt(aWidth) / parseInt(aHeight);
-    } else if (dataSrc == src && img.naturalWidth > 0 && img.naturalHeight > 0) {
-      aspect = img.naturalWidth / img.naturalHeight;
-    }
-    return aspect;
-  };
-  let slides = document.createElement("div");
-  slides.className = "manga-slides";
-  let pages = [];
-  imgs.forEach(function (img, idx) {
-    if (getAspect(img) > 1) {
-      img.className = "manga-slides-wide";
-      pages.push(idx);
-      pages.push(idx);
-    } else {
-      img.className = "manga-slides-half";
-      pages.push(idx);
-    }
-    slides.insertBefore(img, slides.firstChild);
-  });
-  let setAspect = function (img) {
-    if (!img.complete) {
-      img.addEventListener("load", (e) => setAspect(e.target));
-      return;
-    }
-    let aspect = getAspect(img);
-    if (aspect > 1) {
-      aspect = aspect / 2;
-    }
-    aspect = Math.round(aspect * 1000) / 1000;
-    let currentAspect = document.documentElement.style.getPropertyValue("--manga-image-aspect");
-    if (Math.abs(currentAspect - aspect) < 0.001) {
-      return;
-    }
-    console.log("set-aspect", aspect);
-    document.documentElement.style.setProperty("--manga-image-aspect", aspect);
-  };
-  sliding.appendChild(slides);
-  pageprogress.appendChild(pageprogressbar);
-  wrapper.appendChild(pageprogress);
-  wrapper.appendChild(pagenum);
-  wrapper.appendChild(sliding);
-  viewer.appendChild(wrapper);
 
-  let shower = document.createElement("a");
-  shower.innerHTML = "Show Manga Viewer";
-  let showViewer = function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    viewer.style.display = "block";
-  };
-  let hideViewer = function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    viewer.style.display = "none";
-  };
-  shower.addEventListener("click", showViewer);
-  viewer.addEventListener("dblclick", hideViewer);
-
-  document.body.appendChild(viewer);
-  container.parentNode.insertBefore(shower, container);
+  const slides = new Slides();
+  imgs.forEach(img => slides.addImage(new WrappedImage(img)));
+  document.body.appendChild(slides.elem);
+  container.parentNode.insertBefore(slides.createOpenButton("Open Manga Viewer"), container);
   container.parentNode.removeChild(container);
 
-  let currentPageLeft = 0;
-  let loadPage = function (page) {
-    if (page >= 0 && page < pages.length) {
-      let img = imgs[pages[page]];
-      let dataSrc = img.getAttribute("data-src");
-      let src = img.getAttribute("src");
-      if (dataSrc != src) {
-        img.src = dataSrc;
-      }
-    }
-  };
-  let showPage = function (page) {
-    page = Math.min(pages.length, Math.max(0, page)); // 0 <= page <= n
-    let x = -50 * (pages.length - 1 - page); // -50 * (n - 1) <= x <= 50
-    slides.style.transform = "translateX(" + x + "%)";
-    pagenum.innerHTML = Math.min(pages.length, page + 1) + "/" + pages.length; // 1/n <= pagenum <= n/n
-    pageprogressbar.style.width = (100.0 * Math.min(pages.length, page + 1) / pages.length) + "%"; // 100/n <= width <= 100
-    loadPage(page - 2);
-    loadPage(page - 1);
-    loadPage(page);
-    loadPage(page + 1);
-    loadPage(page + 2);
-    if (page >= 0 && page < pages.length) {
-      setAspect(imgs[pages[page]]);
-    }
-    currentPageLeft = page;
-  };
-  let goNext = (shift) => showPage(currentPageLeft + (shift ? 1 : 2));
-  let goPrev = (shift) => showPage(currentPageLeft - (shift ? 1 : 2));
-  showPage(0);
-
-  pageprogress.addEventListener("click", function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-    let rect = e.currentTarget.getBoundingClientRect();
-    let x = e.clientX - rect.left;
-    let r = 1.0 - x / pageprogress.clientWidth; // 0.0 < r < 1.0
-    // if 0 <= r < 1/n then page = 0
-    // if (n-1)/n <= r < 1 then page = n-1
-    // if r = 1 then page = n
-    let page = Math.floor(r * pages.length); // 0 <= page < n
-    showPage(page);
-  });
-
-  addEventListener("keydown", function (e) {
+  document.body.addEventListener("keydown", (e) => {
     if (e.key == "ArrowRight") {
-      goPrev(e.shiftKey);
+      slides.goPrev(e.shiftKey);
     } else if (e.key == "ArrowLeft") {
-      goNext(e.shiftKey);
+      slides.goNext(e.shiftKey);
     } else if (e.key == "Escape") {
-      hideViewer(e);
+      slides.close();
     }
   });
 
-  let lasttime = 0;
-  sliding.addEventListener("wheel", function (e) {
-    e.preventDefault();
-    if (e.timeStamp - lasttime < wheelholdtime) {
-      return;
-    }
-    lasttime = e.timeStamp;
-    if (e.deltaY < 0) {
-      goPrev(e.shiftKey);
-    } else if (e.deltaY > 0) {
-      goNext(e.shiftKey);
-    }
-  });
+  slides.setOffset(0);
 
 })();
